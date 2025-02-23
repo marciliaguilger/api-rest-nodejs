a995b33e0f5d86b7f16d6d9227ef178e88854f6d
marcilia.guilger@ifood.com.br <marcilia.guilger@ifood.com.br>
2025-02-23 18:40:08 -0300

    create

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # api-rest-nodejs
+
+
+comandos:
+npx tsx src/server.ts -> converte codigo para js sem criar arquivo fisico
+usar o tsc somente em ambiente de desenvolvimento.
---
+++ src/server.ts
@@ -0,0 +1,13 @@
+import fastify from "fastify";
+
+const app = fastify()
+
+app.get('/hello', () => {
+    return 'Hello world!'
+})
+
+app.listen({
+    port: 3000
+}).then(() => {
+    console.log('Server listening on port 3000')
+})
