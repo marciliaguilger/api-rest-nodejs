d487c66b60299f359080499683ffca7a4cc27518
marcilia.guilger@ifood.com.br <marcilia.guilger@ifood.com.br>
2025-03-22 18:01:44 -0300

    install knex and create migrations

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,20 @@
 import fastify from 'fastify'
+import { knex } from './database'
+import crypto from 'node:crypto'
+import { env } from './env'
 
 const app = fastify()
 
-app.get('/hello', () => {
-  return 'Hello world!'
+app.get('/hello', async () => {
+  const transaction = await knex('transactions')
+    .insert({
+      id: crypto.randomUUID(),
+      title: 'Transação de teste',
+      amount: 1000,
+    })
+    .returning('*')
+
+  return transaction
 })
 
 app
